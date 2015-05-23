30bf9d289328af3d1de7b27f331de221b85df616
Goel Kratarth <kratarth@bart4.iro.umontreal.ca>
2015-05-23 12:18:59 -0400

    cost changes

--- cle/layers/cost.py
+++ cle/layers/cost.py
@@ -313,19 +313,40 @@ class biGMMLayer(GaussianLayer):
         s_x = (mu_x + sig_x * z[:,0]).dimshuffle(0,'x')
         s_y = mu_y + sig_y * ( (z[:,0] * corr) + (z[:,1] * T.sqrt(1.-corr**2) ) ).dimshuffle(0,'x')
         s = T.concatenate([binary,s_x,s_y], axis = 1)
-        ipdb.set_trace()
         return s
 
     def argmax_mean(self, X):
+
         mu = X[0]
-        coeff = X[1]
+        sig = X[1]
+        coeff = X[2]
+        corr = X[3]
+        binary = X[4]
         mu = mu.reshape((mu.shape[0],
                          mu.shape[1]/coeff.shape[-1],
                          coeff.shape[-1]))
+        sig = sig.reshape((sig.shape[0],
+                           sig.shape[1]/coeff.shape[-1],
+                           coeff.shape[-1]))
         idx = predict(coeff)
+        
         mu = mu[T.arange(mu.shape[0]), :, idx]
-        return mu
-
+        sig = sig[T.arange(sig.shape[0]), :, idx]
+        corr = corr[T.arange(corr.shape[0]), idx]
+        
+        mu_x = mu[:,0]
+        mu_y = mu[:,1]
+        sig_x = sig[:,0]
+        sig_y = sig[:,1]
+     
+        z = self.theano_rng.normal(size=mu.shape,
+                   avg=0., std=1.,
+                   dtype=mu.dtype)
+        s_x = (mu_x + sig_x * z[:,0]).dimshuffle(0,'x')
+        s_y = (mu_y + sig_y * ( (z[:,0] * corr) + (z[:,1] * T.sqrt(1.-corr**2) ) )).dimshuffle(0,'x')
+        s_t = T.concatenate([binary,s_x,s_y], axis = 1)
+        return s_t, mu
+                
     def sample_mean(self, X):
         mu = X[0]
         sig = X[1]
@@ -362,3 +383,34 @@ class biGMMLayer(GaussianLayer):
         s_t = T.concatenate([binary,s_x,s_y], axis = 1)
         
         return s_t, mu
+
+
+    def sample_all_mean(self, X):
+        mu = X[0]
+        sig = X[1]
+        coeff = X[2]
+        corr = X[3]
+        binary = X[4]
+        mu = mu.reshape((mu.shape[0],
+                         mu.shape[1]/coeff.shape[-1],
+                         coeff.shape[-1]))
+        sig = sig.reshape((sig.shape[0],
+                           sig.shape[1]/coeff.shape[-1],
+                           coeff.shape[-1]))
+        
+        mu_x = mu[:,0,:]
+        mu_y = mu[:,1,:]
+        sig_x = sig[:,0,:]
+        sig_y = sig[:,1,:]
+     
+        z = self.theano_rng.normal(size=mu.shape,
+                   avg=0., std=1.,
+                   dtype=mu.dtype)
+        
+        z_x = z[:,0,:]
+        z_y = z[:,1,:]
+        s_x = T.sum(coeff * (mu_x + sig_x * z_x), axis = -1).dimshuffle(0,'x')
+        s_y = T.sum(coeff * (mu_y + sig_y * ( (z_x * corr) + (z_y * T.sqrt(1.-corr**2) ) )), axis = -1).dimshuffle(0,'x')
+        s_t = T.concatenate([binary,s_x,s_y], axis = 1)
+        
+        return s_t, T.sum(coeff.dimshuffle(0,'x',1) * mu, axis = -1)
